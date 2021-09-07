0d02dbf4e1a02ed9834a897169497a186722218e
PerizatSabitbek <psabitbek@magnumtech.kz>
2021-09-07 15:41:24 +0600

    create components

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCVH9rv6rVHSoO4uqtx8iLxomQwJoRbIS0",
+    authDomain: "clone-challenge-9eb36.firebaseapp.com",
+    projectId: "clone-challenge-9eb36",
+    storageBucket: "clone-challenge-9eb36.appspot.com",
+    messagingSenderId: "649628153598",
+    appId: "1:649628153598:web:990d6bead70e0430a317af",
+    measurementId: "G-9NJJLF7TR6"
+  };
