99d5dcae7665df22b515ecfd14a5f4cf505f0421
PerizatSabitbek <psabitbek@magnumtech.kz>
2021-09-07 17:27:33 +0600

    add subtotal component

--- amazon-clone/src/components/Checkout.js
+++ amazon-clone/src/components/Checkout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Checkout.css";
+import Subtotal from "./Subtotal";
 
 function Checkout() {
   return (
@@ -13,7 +14,7 @@ function Checkout() {
         <h2 className="checkout__title">Your shopping basket</h2>
       </div>
       <div className="checkout__right">
-          <h2>The subtotal will go here</h2>
+        <Subtotal />
       </div>
     </div>
   );
